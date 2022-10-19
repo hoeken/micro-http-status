9af2d658b436c6dba5abd7ce3844d13a8159300e
Rene Zeldenthuis <rene@zeldenthuis.eu>
2022-10-19 20:47:37 +0200

    Corrected ino

--- examples/basic.ino
+++ examples/basic.ino
@@ -9,12 +9,12 @@
 void setup()
 {
     auto http_server_error_result = 501;
-    Serial.println("Result: %s", http_success(http_server_error_result) ? "Success" : "Failed");
-    Serial.println("Reason: %s %s", http_status_group(http_server_error_result),  http_status_reason(http_server_error_result));
+    Serial.print("Result: "); Serial.println(http_success(http_server_error_result) ? "Success" : "Failed");
+    Serial.print("Reason: "); Serial.print(http_status_group(http_server_error_result)); Serial.println(http_status_reason(http_server_error_result));
 
     auto http_server_success_result = 201;
-    Serial.println("Result: %s", http_success(http_server_success_result) ? "Success" : "Failed");
-    Serial.println("Reason: %s %s", http_status_group(http_server_success_result),  http_status_reason(http_server_error_result));
+    Serial.print("Result: "); Serial.println(http_success(http_server_success_result) ? "Success" : "Failed");
+    Serial.print("Reason: "); Serial.print(http_status_group(http_server_success_result)); Serial.println(http_status_reason(http_server_success_result));
 }
 
 void loop()
